9d9496de82ecc4a7823fbbb4ccbb4733852beff3
hospodskej <pavelpatockaa@gmail.com>
2026-09-01 09:24:53 +0200

    Patch 0.7.6

--- db/schema.rb
+++ db/schema.rb
@@ -13,56 +13,56 @@
 ActiveRecord::Schema[8.1].define(version: 2026_08_28_120000) do
   create_table "faq_items", force: :cascade do |t|
     t.text "content"
+    t.text "content_de"
     t.datetime "created_at", null: false
     t.string "image"
     t.string "title"
-    t.datetime "updated_at", null: false
     t.string "title_de"
-    t.text "content_de"
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone"
-    t.boolean "newsletter_opt_in", default: false, null: false
-    t.string "billing_street"
     t.string "billing_city"
-    t.string "billing_zip"
     t.string "billing_country"
-    t.boolean "company_purchase", default: false, null: false
-    t.string "company_name"
-    t.string "company_ico"
+    t.string "billing_street"
+    t.string "billing_zip"
     t.string "company_dic"
-    t.boolean "delivery_address_different", default: false, null: false
-    t.string "delivery_street"
-    t.string "delivery_city"
-    t.string "delivery_zip"
-    t.string "delivery_country"
-    t.string "shipping_method"
-    t.string "payment_method"
-    t.text "items_snapshot", null: false
-    t.integer "subtotal_czk", null: false
-    t.integer "vat_czk", null: false
-    t.integer "total_czk", null: false
-    t.string "locale", default: "cs", null: false
+    t.string "company_ico"
+    t.string "company_name"
+    t.boolean "company_purchase", default: false, null: false
     t.datetime "created_at", null: false
+    t.boolean "delivery_address_different", default: false, null: false
+    t.string "delivery_city"
+    t.string "delivery_country"
+    t.string "delivery_street"
+    t.string "delivery_zip"
+    t.string "email"
+    t.string "first_name"
+    t.text "items_snapshot", null: false
+    t.string "last_name"
+    t.string "locale", default: "cs", null: false
+    t.boolean "newsletter_opt_in", default: false, null: false
+    t.string "payment_method"
+    t.string "phone"
+    t.string "shipping_method"
+    t.integer "subtotal_czk", null: false
+    t.integer "total_czk", null: false
     t.datetime "updated_at", null: false
+    t.integer "vat_czk", null: false
   end
 
   create_table "pricelist_items", force: :cascade do |t|
     t.string "category"
     t.datetime "created_at", null: false
     t.string "details"
-    t.string "item_name"
-    t.string "price"
-    t.string "subcategory"
-    t.datetime "updated_at", null: false
-    t.string "item_name_de"
     t.string "details_de"
+    t.string "item_name"
+    t.string "item_name_de"
+    t.string "price"
     t.string "price_de"
+    t.string "subcategory"
     t.string "subcategory_de"
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -70,39 +70,39 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_28_120000) do
     t.string "image"
     t.string "link"
     t.text "text"
-    t.string "title"
-    t.datetime "updated_at", null: false
-    t.string "title_de"
     t.text "text_de"
+    t.string "title"
+    t.string "title_de"
+    t.datetime "updated_at", null: false
   end
 
   create_table "promos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "feature_1"
+    t.string "feature_1_de"
     t.string "feature_2"
+    t.string "feature_2_de"
     t.string "feature_3"
+    t.string "feature_3_de"
     t.string "image"
     t.string "link"
     t.string "price"
-    t.string "title"
-    t.datetime "updated_at", null: false
-    t.string "title_de"
-    t.string "feature_1_de"
-    t.string "feature_2_de"
-    t.string "feature_3_de"
     t.string "price_de"
+    t.string "title"
+    t.string "title_de"
+    t.datetime "updated_at", null: false
   end
 
   create_table "services", force: :cascade do |t|
     t.string "button_path"
     t.string "button_text"
+    t.string "button_text_de"
     t.text "content"
+    t.text "content_de"
     t.datetime "created_at", null: false
     t.text "images"
     t.string "title"
-    t.datetime "updated_at", null: false
     t.string "title_de"
-    t.text "content_de"
-    t.string "button_text_de"
+    t.datetime "updated_at", null: false
   end
 end
